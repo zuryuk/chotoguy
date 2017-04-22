d550d12fe25c1baf50a9637f5afb5d73ef6fca34
zuryuk <luukkanen@gmail.com>
2017-04-22 22:06:06 +0300

    Whole new game

--- Assets/Code/PlayerBehaviour.cs
+++ Assets/Code/PlayerBehaviour.cs
@@ -3,16 +3,20 @@ using System.Collections;
 
 public class PlayerBehaviour : MonoBehaviour {
 
-	public float mvspd;
-	public float jmpheight;
-	public float bltSpd;
+	[SerializeField]
+	private float mvspd;
+	[SerializeField]
+	private float jmpheight;
+	[SerializeField]
+	private float bltSpd;
 
 	private bool doublejump;
-	private float maxspd = 5;
+	private bool direction; // true = left, false = right
 
 	public Rigidbody2D Kirves;
 	private Rigidbody2D Player;
 	public LayerMask Ground;
+	private Animator animator;
 
 	private Vector2 s;
 	private Vector2 start;
@@ -20,59 +24,64 @@ public class PlayerBehaviour : MonoBehaviour {
 	private Vector3 Pos;
 
 	void Start () {
-		Player = GetComponent<Rigidbody2D> ();
+		Player = GetComponentInChildren<Rigidbody2D> ();
 		Ground = LayerMask.GetMask ("Ground");
+		animator = GetComponentInChildren<Animator> ();
+		direction = true;
 		doublejump = true;
 		start = transform.position;
 	}
-		
+	void FixedUpdate (){
+		float horizontal = Input.GetAxis("Horizontal");
+		HandleMovement (horizontal);
+		Flip (horizontal);
+	}
+
 	void Update ()
 	{
-		if (Player.velocity.magnitude > maxspd) {
-			
-		} 
-		else if (Input.GetKey (KeyCode.Space) && IsGrounded ()) {
-			Player.AddForce (Vector2.up * jmpheight, ForceMode2D.Impulse);
-			doublejump = true;
-		} 
-		else if (Input.GetKeyDown (KeyCode.Space) && doublejump) {
-			Player.AddForce (Vector2.up * jmpheight*2, ForceMode2D.Impulse);
-			doublejump = false;
-		} 
-		else if (Input.GetKey (KeyCode.A)) {
-			Player.AddForce ((Vector2.left) * mvspd, ForceMode2D.Impulse);
-		}
-		else if (Input.GetKey (KeyCode.D)) {
-			Player.AddForce (Vector2.right * mvspd, ForceMode2D.Impulse);
-		}
+
+
 		if (Input.GetKey (KeyCode.R)) {
 			Player.velocity = Vector2.zero;
 			transform.position = start;
 		}
-		else if (Input.GetKeyDown (KeyCode.F)) {
+		if (Input.GetKey(KeyCode.Space) && IsGrounded()) {
+			doublejump = true;
+			Player.AddForce (Vector2.up, ForceMode2D.Impulse);
+			animator.SetTrigger ("jump");
+		}
+		if (Input.GetKey(KeyCode.Space) && doublejump) {
+				doublejump = false;
+			Player.AddForce (Vector2.up *jmpheight, ForceMode2D.Impulse);
+
+		}
+		if (Input.GetMouseButtonDown (0)) {
 			ThrowAxe ();
 		}
+	}
 
-		if (Input.GetMouseButtonDown (0)) {
-			Vector3 MousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
-			Debug.DrawLine (transform.position, MousePos);
-			Vector3 Dir = MousePos - transform.position;
-			Rigidbody2D KirvesClone = (Rigidbody2D) Instantiate (Kirves, transform.position, transform.rotation);
-			KirvesClone.transform.LookAt (Camera.main.ScreenToWorldPoint(MousePos));
-			KirvesClone.AddForce (Dir * bltSpd, ForceMode2D.Impulse);
-			KirvesClone.AddTorque(-50);
+	private void ThrowAxe() {
+		Vector3 MousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		Debug.DrawLine (transform.position, MousePos);
+		Vector3 Dir = MousePos - transform.position;
+		Rigidbody2D KirvesClone = (Rigidbody2D) Instantiate (Kirves, transform.position, transform.rotation);
+		KirvesClone.transform.LookAt (Camera.main.ScreenToWorldPoint(MousePos));
+		KirvesClone.AddForce (Dir * bltSpd, ForceMode2D.Impulse);
+		KirvesClone.AddTorque(-50);
+	}
+	private void HandleMovement(float horizontal){
+		Player.AddForce(Vector2.right * horizontal, ForceMode2D.Impulse);
+
+		animator.SetFloat ("Direction", Mathf.Abs(horizontal));
+	}
+	private void Flip(float horizontal){
+		if ((horizontal > 0 && direction) || (horizontal < 0 && !direction)) {
+			direction = !direction;
+			Vector3 theScale = transform.localScale;
+			theScale.x *= -1;
+			transform.localScale = theScale;
 		}
-
-
 	}
-
-	private void ThrowAxe(){
-
-		Rigidbody2D KirvesClone = (Rigidbody2D) Instantiate (Kirves, s, transform.rotation);
-		KirvesClone.AddForce (Vector2.right * bltSpd, ForceMode2D.Impulse);
-		KirvesClone.AddTorque(-200);
-	}
-
 	private bool IsGrounded(){
 		s = transform.position;
 		s.y = GetComponent<Collider2D> ().bounds.min.y + 0.1f;
